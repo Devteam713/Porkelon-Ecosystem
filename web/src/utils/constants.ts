f0d1cf16ef3decfed7824e326ffbaf5393cfad0c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-10-17 22:46:07 -0500

    Update constants.ts

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -11,9 +11,47 @@ export const CHAIN_ID: number = 80002; // Amoy testnet (use 137 for mainnet)
 export const PRESALE_CONTRACT: `0x${string}` = '0xYourPresaleContractAddressHere';
 export const TOKEN_CONTRACT: `0x${string}` = '0x7f024bd81c22dafae5ecca46912acd94511210d8';
 export const STAKING_CONTRACT: `0x${string}` = '0xYourStakingContractAddressHere';
-export const LIQUIDITY_CONTRACT: `0x${string}` = '0xYourLiquidityContractAddressHere'; // e.g., Uniswap Router
+export const LIQUIDITY_CONTRACT: `0x${string}` = '0xYourLiquidityContractAddressHere';
+export const TIMELOCK_CONTRACT: `0x${string}` = '0xYourTimelockContractAddressHere';
+export const UNISWAP_ROUTER: `0x${string}` = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'; // QuickSwap
 export const PRICE_PER_PORK: number = 0.00005; // MATIC per PORK
+
 export const ABI: ContractMethod[] = [
+  // Porkelon (ERC20 Upgradeable)
+  {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'initialize',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
   // Presale
   {
     inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
@@ -29,19 +67,18 @@ export const ABI: ContractMethod[] = [
     stateMutability: 'view',
     type: 'function',
   },
-  // Token
   {
-    inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
-    name: 'mint',
+    inputs: [],
+    name: 'endPresale',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
+    inputs: [],
+    name: 'withdraw',
+    outputs: [],
+    stateMutability: 'nonpayable',
     type: 'function',
   },
   // Staking
@@ -53,8 +90,29 @@ export const ABI: ContractMethod[] = [
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
+    name: 'withdrawStake',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'claimRewards',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
     name: 'stakedBalance',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
+    name: 'getPendingRewards',
     outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
     stateMutability: 'view',
     type: 'function',

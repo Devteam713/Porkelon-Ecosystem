fc402b1c89be0af1168ba566a6121f0be04cb1c8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-10-17 22:09:17 -0500

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Optional RainbowKit setup (uncomment to enable)
+// import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+// import { WagmiProvider } from 'wagmi';
+// import { config } from './wagmi-config';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <WagmiProvider config={config}>
+      <RainbowKitProvider> */}
+        <App />
+      {/* </RainbowKitProvider>
+    </WagmiProvider> */}
   </React.StrictMode>
 );
-createRoot(document.getElementById("root")!).render(<App />);

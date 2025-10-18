45558ca13eba83599f8adb68ed75ee6ffba02400
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-10-17 21:13:00 -0500

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import MintForm from "./components/MintForm";
 import StakeWidget from "./components/StakeWidget";
 import AddLiquidity from "./components/AddLiquidity";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 export default function App() {
   return (
     <div style={{ padding: 24 }}>
